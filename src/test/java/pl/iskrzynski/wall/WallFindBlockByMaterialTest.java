c652d0180dda5faa34991249eaa6fc71c26c0711
SI <szymon@iskrzynski.pl>
2022-11-11 22:28:37 +0100

    Added shouldReturnEmptyListMaterialNoExistInWall test

--- src/test/java/pl/iskrzynski/wall/WallFindBlockByMaterialTest.java
+++ src/test/java/pl/iskrzynski/wall/WallFindBlockByMaterialTest.java
@@ -10,12 +10,23 @@ import static pl.iskrzynski.wall.WallAssert.buildBlock;
 
 public class WallFindBlockByMaterialTest {
 
+    Block block1 = buildBlock("blue", "wood");
+    Block block2 = buildBlock("green", "wood");
+    Block block3 = buildBlock("blue", "steel");
+    Block block4 = buildBlock("green", "steel");
+    List<Block> blockList = List.of(block1,block2,block3,block4);
     Wall emptyWall = new Wall(new ArrayList<>());
     List<Block> emptyList = new ArrayList<>();
+    Wall noEmptyWall = new Wall(blockList);
 
     @Test
     void shouldReturnEmptyListFromEmptyWall() {
         assertThat(emptyWall.findBlocksByMaterial("wood")).isEqualTo(emptyList);
         assertThat(emptyWall.findBlocksByMaterial("steel")).isEqualTo(emptyList);
+    }
+
+    @Test
+    void shouldReturnEmptyListMaterialNoExistInWall() {
+        assertThat(noEmptyWall.findBlocksByMaterial("plastic")).isEqualTo(emptyList);
     }
 }
