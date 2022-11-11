3cac346c6c4af05b800d4ec790e7beb91a53e741
SI <szymon@iskrzynski.pl>
2022-11-11 18:33:47 +0100

    Added CompositeBlock interface implementation

--- src/test/java/pl/iskrzynski/wall/WallAssert.java
+++ src/test/java/pl/iskrzynski/wall/WallAssert.java
@@ -1,20 +1,44 @@
 package pl.iskrzynski.wall;
 
 
+import java.util.List;
+
 class WallAssert {
-    static Block buildBlock(String aColor, String aMaterial){
+    static Block buildBlock(String aColor, String aMaterial) {
         return new Block() {
             private final String color = aColor;
             private final String material = aMaterial;
+
             @Override
             public String getColor() {
                 return color;
             }
+
             @Override
             public String getMaterial() {
                 return material;
             }
+        };
+    }
 
+    static CompositeBlock buildCompositeBlock(List<Block> aBlocks) {
+        return new CompositeBlock() {
+            final List<Block> blocks = aBlocks;
+
+            @Override
+            public List<Block> getBlocks() {
+                return blocks;
+            }
+
+            @Override
+            public String getColor() {
+                return null;
+            }
+
+            @Override
+            public String getMaterial() {
+                return null;
+            }
         };
     }
 
