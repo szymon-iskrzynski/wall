ecace98576a1410d0613c186f1c5e9ec59d244e0
SI <szymon@iskrzynski.pl>
2022-11-11 19:28:51 +0100

    Added shouldReturnFiveElements test

--- src/test/java/pl/iskrzynski/wall/WallCountTest.java
+++ src/test/java/pl/iskrzynski/wall/WallCountTest.java
@@ -7,10 +7,13 @@ import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static pl.iskrzynski.wall.WallAssert.buildBlock;
+import static pl.iskrzynski.wall.WallAssert.buildCompositeBlock;
 
 public class WallCountTest {
     Block block1 = buildBlock("blue", "wood");
     List<Block> threeElementsBlockList = List.of(block1,block1,block1);
+    CompositeBlock threeElementsCompositeBlock = buildCompositeBlock(threeElementsBlockList);
+    List<Block> fiveElementsBlockList = List.of(threeElementsCompositeBlock,block1,block1);
 
     @Test
     void shouldReturnZero() {
@@ -22,6 +25,12 @@ public class WallCountTest {
     void shouldReturnThreeElements() {
         Wall wall = new Wall(threeElementsBlockList);
         assertThat(wall.count()).isEqualTo(3);
+    }
+
+    @Test
+    void shouldReturnFiveElements() {
+        Wall wall = new Wall(fiveElementsBlockList);
+        assertThat(wall.count()).isEqualTo(5);
     }
 
 }
